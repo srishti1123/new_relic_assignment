07a0722debeb883a592abbca35b716586b5fe28c
srishti1123 <139761998+srishti1123@users.noreply.github.com>
2024-05-13 15:06:50 +0530

    Update index.js

--- nerdlets/abc-nerdlet/index.js
+++ nerdlets/abc-nerdlet/index.js
@@ -1,29 +1,51 @@
 import React from "react";
  
-import { AreaChart, LineChart ,PieChart,BarChart,Scatter} from "nr1";
+import { AreaChart, LineChart, PieChart,BarChart } from "nr1";
  
 import { Grid, GridItem } from "nr1";
  
 const index = () => {
-return (
-<div className="heading">
-<h1>New Relic Assignmnet</h1>
-<AreaChart
-accountId={4438260}
-query="SELECT average(transmitBytesPerSecond) AS `Transmit bytes per second`, average(receiveBytesPerSecond) AS `Receive bytes per second` FROM NetworkSample WHERE (entityGuid = 'NDQzODI2MHxJTkZSQXxOQXwyNDEzMzU5OTc4MDM1MjEwOTU4') TIMESERIES AUTO"
-fullWidth
-/>
-<LineChart
-accountId={4438260}
-query="SELECT average(transmitBytesPerSecond) AS `Transmit bytes per second`, average(receiveBytesPerSecond) AS `Receive bytes per second` FROM NetworkSample WHERE (entityGuid = 'NDQzODI2MHxJTkZSQXxOQXwyNDEzMzU5OTc4MDM1MjEwOTU4') TIMESERIES AUTO"
-fullWidth
-/>
-<BarChart
+  return (
+    <div className="heading"><P>Hi! everyone</P>
+ 
+   <Grid>
+   
+    <GridItem columnSpan={5}>
+    <AreaChart
+          accountId={4438260}
+           query= "SELECT average(transmitBytesPerSecond) AS `Transmit bytes per second`, average(receiveBytesPerSecond) AS `Receive bytes per second` FROM NetworkSample WHERE (entityGuid = 'NDQzODI2MHxJTkZSQXxOQXwyNDEzMzU5OTc4MDM1MjEwOTU4') TIMESERIES AUTO"
+             fullWidth
+          />
+          </GridItem>
+ 
+          <GridItem columnSpan={5}>
+          <LineChart
             accountId={4438260}
-            query= "SELECT average(loadAverageOneMinute) AS `1 minute`, average(loadAverageFiveMinute) AS `5 minutes`, average(loadAverageFifteenMinute) AS `15 minutes` FROM SystemSample WHERE (entityGuid = 'NDQzODI2MnxJTkZSQXxOQXwxNDc5NjY3NjY2MzkzNjIzODQ1') TIMESERIES AUTO"
+           query ="SELECT average(loadAverageOneMinute) AS `1 minute`, average(loadAverageFiveMinute) AS `5 minutes`, average(loadAverageFifteenMinute) AS `15 minutes` FROM SystemSample WHERE (entityGuid = 'NDQzODI2MHxJTkZSQXxOQXwyNDEzMzU5OTc4MDM1MjEwOTU4') TIMESERIES AUTO      " 
+                 fullWidth
+          />
+          </GridItem>
+ 
+          <GridItem columnSpan={5}>
+         <PieChart
+            accountId={4438260}
+            query= "SELECT average(transmitBytesPerSecond) AS `Transmit bytes per second`, average(receiveBytesPerSecond) AS `Receive bytes per second` FROM NetworkSample WHERE (entityGuid = 'NDQzODI2MHxJTkZSQXxOQXwyNDEzMzU5OTc4MDM1MjEwOTU4') TIMESERIES AUTO "
             fullWidth
-/>
-</div> 
-
-)};
+          />
+          </GridItem>
+ 
+          <GridItem columnSpan={5}>
+            <BarChart
+            accountId={4438260}
+            query ="SELECT average(transmitBytesPerSecond) AS `Transmit bytes per second`, average(receiveBytesPerSecond) AS `Receive bytes per second` FROM NetworkSample WHERE (entityGuid = 'NDQzODI2MHxJTkZSQXxOQXwyNDEzMzU5OTc4MDM1MjEwOTU4') TIMESERIES AUTO     "
+            fullWidth
+          />
+          </GridItem>
+         
+   
+     </Grid>
+    </div>
+  );
+};
+ 
 export default index;
